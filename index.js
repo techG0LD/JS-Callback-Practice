c31e7f8af3e9fd878fdee3fda9bf16d8a1732bbd
techG0LD <puigster956@gmail.com>
2023-05-06 20:01:18 -0700

    launch

--- index.js
+++ index.js
@@ -2,7 +2,92 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+// move(newImage('assets/tree.png')).withArrowKeys(200,450)
+
+function handleDirectionChange(direction){
+
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+} 
+
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+} 
+
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+} 
+
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+} 
+
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+} 
+
+}
+
+
+move(character).withArrowKeys(100,250,handleDirectionChange)
+
+// let x = left;
+// let y = bottom;
+ 
+
+// function moveCharacter(){
+//      if (direction === 'west') {
+//     x = x -1
+// }
+
+// if (direction === 'east') {
+//     x = x + 1
+// }
+
+// if (direction === 'south') {
+//     y = y - 1
+// }
+
+// if (direction === 'north') {
+//     y = y + 1
+// }
+
+// character.style.left = x + 'px'
+// character.style.bottom = y + 'px'
+// }
+ 
+
+
+// setInterval(moveCharacter,1)
+
+// document.addEventListener('keydown',function(e){
+
+//     if(e.repeat) return;
+
+//     if(e.key === 'ArrowLeft'){
+//         direction = 'west'
+//     }
+
+//     if(e.key === 'ArrowUp'){
+//         direction = 'north'
+//     }
+
+//     if(e.key === 'ArrowRight'){
+//         direction = 'east'
+//     }
+
+//     if(e.key === 'ArrowDown'){
+//         direction = 'south'
+//     }
+// })
+
+// document.addEventListener('keyup',function(e){
+//     direction = null;
+// })
+
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
--- move.js
+++ move.js
@@ -6,7 +6,72 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left,bottom,callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveCharacter(){
+            if (direction === 'west') {
+           x = x -1
+        }
+       
+        if (direction === 'east') {
+            x = x + 1
+        }
+       
+        if (direction === 'south') {
+            y = y - 1
+        }
+       
+        if (direction === 'north') {
+           y = y + 1
+        }
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+       
+       
+       }
+        
+       
+       
+       setInterval(moveCharacter,1)
+       
+       document.addEventListener('keydown',function(e){
+       
+           if(e.repeat) return;
+       
+           if(e.key === 'ArrowLeft'){
+               direction = 'west'
+           }
+       
+           if(e.key === 'ArrowUp'){
+               direction = 'north'
+           }
+       
+           if(e.key === 'ArrowRight'){
+               direction = 'east'
+           }
+       
+           if(e.key === 'ArrowDown'){
+               direction = 'south'
+           }
+           
+           callback(direction)
+       })
+       
+       document.addEventListener('keyup',function(e){
+           direction = null;
+           callback(direction)
+       })
+       
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
